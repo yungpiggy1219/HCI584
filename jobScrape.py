db4e6badc5ba144c4cc2c0cf18d0387d75f36749
yungchu <yungchc1@uci.edu>
2023-10-29 18:14:42 -0500

    Linkedin scrape working now

--- jobScrape.py
+++ jobScrape.py
@@ -4,26 +4,45 @@ import time
 import os.path
 
 # Scrape UX jobs from from Linkedin
-print("Scraping UX jobs from Linkedin...")
-LIuxJobs = scrape_jobs(
-    site_name=["linkedin"],
-    search_term="ux",
-    location="60640",
-    results_wanted="1000",
-    # location='USA',
-    # country_indeed='USA'  # only needed for indeed
-)
-print(f"Found {len(LIuxJobs)} UX jobs on Linkedin")
+print("Scraping UX jobs from Linkedin")
 
-# Check if file exists
-if not os.path.isfile("jobs.csv"):
-    # Create file if doesn't exist  
-    print("Results saved to a new file jobs.csv")
-    LIuxJobs.to_csv("jobs.csv", index=False)
-else:
-    # Add results to existing file
-    print("Adding results in existing file jobs.csv")
-    LIuxJobs.to_csv("jobs.csv", mode="a", index=False, header=False)
+offset = 0
+chunk_size = 50
+chunk = 0
+num_chunks = 3  # change this to get more
+
+#for chunk in range(0, num_chunks):
+while True:
+    try:
+        print(f"Scraping chunk {chunk+1}...")
+        jobs = scrape_jobs(
+            site_name=["Linkedin"],  # neither linkedin nor indeed worked for me
+            search_term="ux",
+            # location="60640",
+            results_wanted=chunk_size,
+            offset=offset,
+            # location='USA',
+            # country_indeed='USA'  # only needed for indeed
+        )
+    except:
+        print("End of Search. Scraping Stopped.")
+        break
+
+    else:
+        print(f"Found {len(jobs)} UX jobs on Linkedin in chunk {chunk+1}")
+        offset += chunk_size
+        chunk += 1
+
+    # Check if file exists
+    if not os.path.isfile("jobs.csv"):
+        # Create file if doesn't exist  
+        print("Results saved to a new file jobs.csv")
+        jobs.to_csv("jobs.csv", index=False)
+    else:
+        # Add results to existing file
+        print("Adding results in existing file jobs.csv")
+        jobs.to_csv("jobs.csv", mode="a", index=False, header=False)
+    time.sleep(5)
 
 # Read jobs.csv
 df_state = pd.read_csv("jobs.csv")
@@ -34,32 +53,5 @@ Dup_Rows = df_state[df_state.duplicated()]
 print("Number of duplicate Rows: ", len(Dup_Rows))
 
 DF_RM_DUP = df_state.drop_duplicates(keep='first')
-print('Number of job after duplicate removed: ', len(DF_RM_DUP))
+print('Number of jobs after duplicate removed: ', len(DF_RM_DUP))
 DF_RM_DUP.to_csv("jobs.csv", index=False)
-
-""" 
-# Scrape HCI jobs from from Linkedin
-print("Scraping HCI Jobs...")
-time.sleep(10)
-LIhciJobs = scrape_jobs(
-    site_name=["linkedin"],
-    search_term="human computer interaction",
-    results_wanted="100",
-    location='USA',
-    country_indeed='USA'  # only needed for indeed
-)
-print(f"Found {len(LIhciJobs)} HCI jobs")
-LIhciJobs.to_csv("jobs.csv", index=False)
-
-# Scrape HF jobs from from Linkedin
-print("Scraping Human Factors Jobs on Linkedin...")
-time.sleep(10)
-LIhfJobs = scrape_jobs(
-    site_name=["linkedin"],
-    search_term="human factors",
-    results_wanted="100",
-    location='USA',
-    country_indeed='USA'  # only needed for indeed
-)
-print(f"Found {len(LIhfJobs)} Human Factors jobs")
-LIhfJobs.to_csv("jobs.csv", index=False) """
